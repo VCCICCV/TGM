cc9d752236ef07b7d77376ee99bd56d029dfa7b2
VCCICCV <chaochen497@gmail.com>
2024-07-17 03:15:00 +0800

    pay&marketing

---
+++ tgm-marketing/tgm-marketing-interfaces/src/test/java/com/cci/tgm/marketing/interfaces/TgmMarketingInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cci.tgm.marketing.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgmMarketingInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
