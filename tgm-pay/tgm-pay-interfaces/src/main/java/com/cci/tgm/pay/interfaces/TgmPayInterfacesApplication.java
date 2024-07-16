f1634b44248b1f4163fbd60cb618560755dbe434
VCCICCV <chaochen497@gmail.com>
2024-07-16 13:47:26 +0800

    first

---
+++ tgm-pay/tgm-pay-interfaces/src/main/java/com/cci/tgm/pay/interfaces/TgmPayInterfacesApplication.java
@@ -0,0 +1,13 @@
+package com.cci.tgm.pay.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgmPayInterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgmPayInterfacesApplication.class, args);
+	}
+
+}
